a275d885d147a76781b19dc32b94f822ad15a517
KaungMyatK <kaungmyat.dev8@gmail.com>
2025-07-03 20:27:40 +0630

    add router

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="w-full border-b">
+      <div className="container flex items-center h-16"></div>
+    </header>
+  );
+}
+
+export default Header;
